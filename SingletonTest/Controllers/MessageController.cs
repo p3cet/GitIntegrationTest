a8275fc9515e4095aca0c929b5fd682965af8706
Przemysław Cicharski <cprzem@gmail.com>
2017-09-30 20:00:22 +0200

    Dodaj pliki projektów.

---
+++ SingletonTest/Controllers/MessageController.cs
@@ -0,0 +1,34 @@
+﻿using SingletonTest.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace SingletonTest.Controllers
+{
+    public class MessageController : Controller
+    {
+        // GET: Message
+        public ActionResult Index()
+        {
+            Message CurrentMessage = Message.Instance;
+            return View();
+        }
+
+        public ActionResult ViewMessage() 
+        {
+            ViewBag.MessageText = Message.MessageText;
+            return View();
+        }
+
+        public ActionResult EditMessage() => View();
+
+        public ActionResult SetMessage(string text)
+        {
+            Message.MessageText = text;
+            ViewBag.MessageText = Message.MessageText;
+            return View();
+        }
+    }
+}
